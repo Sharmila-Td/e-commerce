c3fde7a4cebce7261578614cb4a6afb6dbc7bfca
sharmila-developer <sharmi.webdev@gmail.com>
2024-09-26 08:46:12 -0700

    admin folder created

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,16 +1,19 @@
 import { createContext, useEffect, useState } from "react";
-import {products} from '../assets/assets'
+// import {products} from '../assets/assets'
 import { toast } from "react-toastify";
 import {useNavigate} from 'react-router-dom';
+import axios from 'axios'
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = '₹' 
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([])
     const navigate = useNavigate()
     
     const addToCart = async (itemId, size) => {
@@ -48,6 +51,7 @@ const ShopContextProvider = (props) => {
         return totalCount;
     }
 
+
     const updateQuantity = async (itemId, size, quantity) => {
         let cartData = structuredClone(cartItems);
         cartData[itemId][size] = quantity;
@@ -71,6 +75,23 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const getProductsData = async () => {
+        try{
+            const response = await axios.get(backendUrl + '/api/product/list')
+            if(response.data.success){
+                setProducts(response.data.products)
+            }else{
+                toast.error(response.data.message)
+            }
+        }catch(err){
+            console.log(err)
+            toast.error(err.message)
+        }
+    }
+
+    useEffect(()=>{
+        getProductsData
+    },[])
     const value = {
         products,
         currency, 
@@ -84,7 +105,8 @@ const ShopContextProvider = (props) => {
         getCartCount,
         updateQuantity,
         getCartAmount,
-        navigate
+        navigate,
+        backendUrl
     }
 
     return(
---
+++ server/middleware/adminAuth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const adminAuth = async(req,res,next) => {
+    try{
+        const {token} = req.headers
+        if(!token){
+            return res.json({success:false, message:"Not Authorized login again"})
+        }
+        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET)
+        if(tokenDecode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+            return res.json({success:false, message:"Not Authorized login again"})
+        }
+        next()
+    }catch(err){
+        console.log(err)
+        res.json({success:false, message:err.message})
+    }
+}
+
+export default adminAuth;
