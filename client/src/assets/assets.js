8e4d22435ffe3df897dd740bcb5a1afdd86edabe
sharmila-developer <sharmi.webdev@gmail.com>
2024-09-29 08:44:55 -0700

    insert images

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,6 +2,20 @@ import wtshirt1 from '../assets/women_tshirt1.jpeg';
 import wtshirt2 from '../assets/women_tshirt2.jpeg';
 import wtshirt3 from '../assets/women_tshirt3.jpeg';
 import wtshirt4 from '../assets/women_tshirt4.jpeg';
+import mentshirt1 from '../assets/mentshirt1.jpg';
+import mentshirt2 from '../assets/mentshirt2.jpg';
+import mentshirt3 from '../assets/mentshirt3.jpg';
+import mentshirt4 from '../assets/mentshirt4.jpg';
+import CottonShirt1 from '../assets/CottonShirt1.jpg';
+import CottonShirt2 from '../assets/CottonShirt2.jpg';
+import CottonShirt3 from '../assets/CottonShirt3.jpg';
+import kidsCasual1 from '../assets/kidsCasual1.jpg';
+import kidsCasual2 from '../assets/kidsCasual2.jpg';
+import kidsCasual3 from '../assets/kidsCasual3.jpg';
+import kidsCasual4 from '../assets/kidsCasual4.jpg';
+import newbornTshirt1 from '../assets/newbornTshirt1.jpg';
+import newbornTshirt2 from '../assets/newbornTshirt2.jpg';
+import newbornTshirt3 from '../assets/newbornTshirt3.jpg';
 
 
 
@@ -10,12 +24,21 @@ export const assets = {
     wtshirt2,
     wtshirt3,
     wtshirt4,
+    mentshirt1,
+    mentshirt2,
+    mentshirt3,
+    mentshirt4,
+    CottonShirt1,
+    CottonShirt2,
+    CottonShirt3,
+    kidsCasual1, kidsCasual2, kidsCasual3, kidsCasual4,
+    newbornTshirt1, newbornTshirt2, newbornTshirt3
 }
 
 export const products = [
     {
         id:"1",
-        name:"ok",
+        name:"Women Rounded Tshirt",
         description:"A lightweighted blue tshirt for women, feel comfortable and softy",
         price:500,
         image:[wtshirt1, wtshirt2, wtshirt3, wtshirt4],
@@ -26,48 +49,48 @@ export const products = [
         bestSeller:true
     },
     {
-        id:"a1",
-        name:"Women Rounded Neck T-shirt",
-        description:"",
-        price:450,
-        image:[wtshirt1],
-        category:"Women ",
+        id:"2",
+        name:"Men's Solid Regular Fit Polo",
+        description:"Allen Solly Men's Solid Regular Fit Polo offers comfort and style with a durable, breathable fabric.",
+        price:439,
+        image:[mentshirt1, mentshirt2, mentshirt3, mentshirt4],
+        category:"Men",
+        subCategory:"Topwear",
+        sizes:["S", "M", "L"],
+        date:145698564,
+        bestSeller:true
+    },
+    {
+        id:"3",
+        name:"Cotton Shirt for Man",
+        description:"FREKMAN Top Wear Double Pocket Shirt || Trending Cargo Full Sleeve, Cotton Shirt for Man || Pack of 2",
+        price:1162,
+        image:[CottonShirt1, CottonShirt2, CottonShirt3],
+        category:"Men ",
+        subCategory:"Topwear",
+        sizes:["S", "M", "L"],
+        date:145698565,
+        bestSeller:true
+    },
+    {
+        id:"4",
+        name:"CASUAL TOP",
+        description:"NIYA GIRLS PARTY EMBELLISHED EMBROIDERED FESTIVE CASUAL TOP",
+        price:499,
+        image:[kidsCasual1, kidsCasual2, kidsCasual3, kidsCasual4],
+        category:"Kids",
         subCategory:"Topwear",
         sizes:["S", "M", "L"],
         date:145698563,
         bestSeller:true
     },
     {
-        id:"a1",
-        name:"Women Rounded Neck T-shirt",
-        description:"",
-        price:450,
-        image:[wtshirt1],
-        category:"Women ",
-        subCategory:"Topwear",
-        sizes:["S", "M", "L"],
-        date:145698563,
-        bestSeller:true
-    },
-    {
-        id:"a1",
-        name:"Women Rounded Neck T-shirt",
-        description:"",
-        price:450,
-        image:[wtshirt1],
-        category:"Women ",
-        subCategory:"Topwear",
-        sizes:["S", "M", "L"],
-        date:145698563,
-        bestSeller:true
-    },
-    {
-        id:"a1",
-        name:"Women Rounded Neck T-shirt",
-        description:"",
-        price:450,
-        image:[wtshirt1],
-        category:"Women ",
+        id:"5",
+        name:"Newborn Topwear",
+        description:"baby wish Newborn Birthday T-Shirt for Kids Clothes Unisex Newborn Topwear Outfit Printed Tshirt Half Sleeve Unisex Round Neck Dress for Boy n Girl Lion Brave Prints",
+        price:499,
+        image:[newbornTshirt1, newbornTshirt2, newbornTshirt3],
+        category:"Kids",
         subCategory:"Topwear",
         sizes:["S", "M", "L"],
         date:145698563,
